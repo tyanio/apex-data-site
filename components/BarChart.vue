53562e0d9408776ae3893ee2d5ef6c1ef90df58e
Tyanio <tyanio1996@gmail.com>
2021-02-20 05:47:16 +0900

    vue-chartを試しました

---
+++ components/BarChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Bar } from "vue-chartjs";
+export default {
+  extends: Bar,
+  data() {
+    return {
+      chartdata: {
+        labels: ["january", "Feburary"],
+        datasets: [
+          {
+            label: ["Data One"],
+            backgroundColor: "#f87979",
+            data: [40, 30]
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  }
+};
+</script>
