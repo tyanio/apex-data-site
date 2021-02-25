8ea17490633fb998622350081c464234c38ba658
Tyanio <tyanio1996@gmail.com>
2021-02-25 21:19:37 +0900

    wip

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,20 +1,90 @@
 <script>
-import { Bar } from "vue-chartjs";
+import { Line } from "vue-chartjs";
 export default {
-  extends: Bar,
+  extends: Line,
   data() {
     return {
       chartdata: {
-        labels: ["january", "Feburary"],
         datasets: [
           {
-            label: ["Data One"],
-            backgroundColor: "#f87979",
-            data: [40, 30]
+            label: "Flatline",
+            borderColor: "#64C4A9",
+            steppedLine: true,
+            data: [
+              {
+                x: 0,
+                y: 19
+              },
+              {
+                x: 0.1,
+                y: 38
+              },
+              {
+                x: 0.2,
+                y: 57
+              },
+              {
+                y: 76,
+                x: 0.3
+              },
+              {
+                y: 95,
+                x: 0.4
+              },
+              {
+                y: 114,
+                x: 0.5
+              },
+              {
+                y: 133,
+                x: 0.6
+              },
+              {
+                y: 152,
+                x: 0.7
+              },
+              {
+                y: 171,
+                x: 0.8
+              },
+              {
+                y: 190,
+                x: 0.9
+              },
+              {
+                y: 209,
+                x: 1.0
+              },
+              {
+                y: 228,
+                x: 1.1
+              },
+              {
+                y: 247,
+                x: 1.2
+              },
+              {
+                y: 266,
+                x: 1.3
+              }
+            ]
           }
         ]
       },
       options: {
+        // scales: {
+        //   xAxes: [
+        //     {
+        //       txpe: "linear",
+        //       offset: true,
+        //       ticks: {
+        //         beginAtZero: true,
+        //         max: 2,
+        //         stepSize: 0.1
+        //         }
+        //     }
+        //   ]
+        // },
         responsive: true,
         maintainAspectRatio: false
       }
