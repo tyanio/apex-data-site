67e6cef6c00c95302c1b691cafda7f5d874442c8
Tyanio <tyanio1996@gmail.com>
2021-02-27 05:50:12 +0900

    FlatlineとR-301のデータをグラフに表示しました

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -8,90 +8,154 @@ export default {
         datasets: [
           {
             label: "Flatline",
-            borderColor: "#64C4A9",
+            borderColor: "#6ACEA8",
             steppedLine: true,
             data: [
               {
                 x: 0,
-                y: 19
+                y: 19,
               },
               {
                 x: 0.1,
-                y: 38
+                y: 38,
               },
               {
                 x: 0.2,
-                y: 57
+                y: 57,
               },
               {
+                x: 0.3,
                 y: 76,
-                x: 0.3
               },
               {
+                x: 0.4,
                 y: 95,
-                x: 0.4
               },
               {
+                x: 0.5,
                 y: 114,
-                x: 0.5
               },
               {
+                x: 0.6,
                 y: 133,
-                x: 0.6
               },
               {
+                x: 0.7,
                 y: 152,
-                x: 0.7
               },
               {
+                x: 0.8,
                 y: 171,
-                x: 0.8
               },
               {
+                x: 0.9,
                 y: 190,
-                x: 0.9
               },
               {
+                x: 1.0,
                 y: 209,
-                x: 1.0
               },
               {
+                x: 1.1,
                 y: 228,
-                x: 1.1
+              },
+            ],
+          },
+          {
+            label: "R-301",
+            borderColor: "#F49A4A",
+            steppedLine: true,
+            data: [
+              {
+                x: (1 - 1) / 13.5,
+                y: 1 * 14,
               },
               {
-                y: 247,
-                x: 1.2
+                x: (2 - 1) / 13.5,
+                y: 2 * 14,
               },
               {
-                y: 266,
-                x: 1.3
-              }
-            ]
-          }
-        ]
+                x: (3 - 1) / 13.5,
+                y: 3 * 14,
+              },
+              {
+                x: (4 - 1) / 13.5,
+                y: 4 * 14,
+              },
+              {
+                x: (5 - 1) / 13.5,
+                y: 5 * 14,
+              },
+              {
+                x: (6 - 1) / 13.5,
+                y: 6 * 14,
+              },
+              {
+                x: (7 - 1) / 13.5,
+                y: 7 * 14,
+              },
+              {
+                x: (8 - 1) / 13.5,
+                y: 8 * 14,
+              },
+              {
+                x: (9 - 1) / 13.5,
+                y: 9 * 14,
+              },
+              {
+                x: (10 - 1) / 13.5,
+                y: 10 * 14,
+              },
+              {
+                x: (11 - 1) / 13.5,
+                y: 11 * 14,
+              },
+              {
+                x: (12 - 1) / 13.5,
+                y: 12 * 14,
+              },
+              {
+                x: (13 - 1) / 13.5,
+                y: 13 * 14,
+              },
+              {
+                x: (14 - 1) / 13.5,
+                y: 14 * 14,
+              },
+              {
+                x: (15 - 1) / 13.5,
+                y: 15 * 14,
+              },
+              {
+                x: (16 - 1) / 13.5,
+                y: 16 * 14,
+              },
+              {
+                x: (17 - 1) / 13.5,
+                y: 17 * 14,
+              },
+            ],
+          },
+        ],
       },
       options: {
-        // scales: {
-        //   xAxes: [
-        //     {
-        //       txpe: "linear",
-        //       offset: true,
-        //       ticks: {
-        //         beginAtZero: true,
-        //         max: 2,
-        //         stepSize: 0.1
-        //         }
-        //     }
-        //   ]
-        // },
+        scales: {
+          xAxes: [
+            {
+              type: "linear",
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
         responsive: true,
-        maintainAspectRatio: false
-      }
+        maintainAspectRatio: false,
+      },
     };
   },
   mounted() {
     this.renderChart(this.chartdata, this.options);
-  }
+  },
 };
 </script>
