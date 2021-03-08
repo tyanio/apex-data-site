8cfa5229f05cbeadbdd121f2e22413906042d1e6
Tyanio <tyanio1996@gmail.com>
2021-03-08 17:43:28 +0900

    棒グラフの表示ができました

---
+++ components/BarChart.vue
@@ -0,0 +1,46 @@
+<script>
+import { Bar, mixins } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  // mixins: [mixins.reactiveProp],
+  // props: ["chartData"],
+  data() {
+    return {
+      chartData: {
+        labels: ["January", "February", "March", "April", "May"],
+        datasets: [
+          {
+            label: "Sample1",
+            data: [28, 20, 30, 40, 50],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+};
+</script>
---
+++ pages/200Health.vue
@@ -0,0 +1,13 @@
+<template>
+  <BarChart />
+</template>
+
+<script>
+import BarChart from "@/components/BarChart.vue";
+
+export default {
+  components: {
+    BarChart,
+  },
+};
+</script>
