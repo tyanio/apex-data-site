1d307c6b2a1f5c871e0557e08c30818bf66af686
Tyanio <tyanio1996@gmail.com>
2021-07-09 00:05:16 +0900

    シールドの選択ボタンを用意しました

--- pages/timeToDefeat.vue
+++ pages/timeToDefeat.vue
@@ -1,5 +1,23 @@
 <template>
-  <BarChart v-bind:chart-data="dataCollection" />
+  <v-container>
+    <v-col>
+      <v-btn-toggle>
+        <v-btn icon>
+          <v-img height="48" contain src="/icon_shields/level1.png"></v-img>
+        </v-btn>
+        <v-btn icon>
+          <v-img height="48" contain src="/icon_shields/level2.png"></v-img>
+        </v-btn>
+        <v-btn icon>
+          <v-img height="48" contain src="/icon_shields/level3.png"></v-img>
+        </v-btn>
+        <v-btn icon>
+          <v-img height="48" contain src="/icon_shields/level4.png"></v-img>
+        </v-btn>
+      </v-btn-toggle>
+      <BarChart v-bind:chart-data="dataCollection" />
+    </v-col>
+  </v-container>
 </template>
 
 <script>
